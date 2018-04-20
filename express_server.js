b4b57339f11ba3f47247b66870714fe04e972b36
Nina Yujiri <ninayujiri@gmail.com>
2018-04-20 23:18:36 +0000

    switched over to cookie sessions for encrypted cookies

--- express_server.js
+++ express_server.js
@@ -6,10 +6,10 @@ app.set("view engine", "ejs");
 
 
 // Middleware
-var cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
-  keys: 'some-long-secret',
+  keys: ['key1', 'key2']
 }));
 
 const bodyParser = require("body-parser");
@@ -82,59 +82,24 @@ app.post("/register", (req, res) => {
 
   if (inputEmail === "" && inputPassword === "") {
     res.status(400).send('Please enter an email and password');
-  } else if (inputPassword === "") {
-    res.status(400).send('Please enter a password');
-  } else if (inputEmail === "") {
-    res.status(400).send("Please enter an email");
+  } else if (inputPassword === "" || inputEmail === "") {
+    res.status(400).send('Please enter both a username and password');
   } else {
     for (let userId in users) {
       if (inputEmail === users[userId].email) {
         emailFound = true;
       }
-    } if (emailFound === true) {
+    }
+    if (emailFound === true) {
       res.status(400).send('Email has already been taken');
     } else {
       users[id] = { id: id, email: req.body.email, password: hashedPassword };
-      // res.cookie('user_id', users[id].id);
       req.session.user_id = users[id].id;
-      console.log("old: ", users[id].id);
-      console.log("just id: ", id);
       res.redirect("/urls");
     }
   }
-  console.log("users: ", users);
+  // console.log("users: ", users);
 });
-
-
-
-
-
-
-
-app.post('/register', (req, res) => {
-  // checks if email or password is empty
-  if (req.body.email === '' || req.body.password === '') {
-    res.status(400).send('Email or password empty. <a href="/register">Go Back</a>');
-  } else if (regChecker(req.body.email)) {
-    res.status(400).send('Email already in database. Forgot? Well too bad. Make a new one. <a href="/register">Go Back</a>');
-  } else {
-    // generates a new id and assigns it into database
-    let newUserId = generateRandomNumber();
-    users[newUserId] = {
-      id: newUserId,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10)
-    };
-    req.session.user_id = newUserId;
-    res.redirect('/urls');
-  }
-});
-
-
-
-
-
-
 
 
 // Login Page
@@ -148,9 +113,12 @@ app.post("/login", (req, res) => {
   let userFound = findUser(inputEmail);
   const hashedPassword = bcrypt.hashSync(inputPassword, 10);
 
+  if (inputEmail === null || inputPassword === null) {
+    res.status(400).send('Please fill in both email and password fields. <a href = "/login">Try again.</a>');
+  }
+
   if (userFound && bcrypt.compareSync(inputPassword, hashedPassword)) {
-    // res.cookie('user_id', userFound.id);
-    res.session.user_id = userFound.id;
+    req.session.user_id = userFound.id;
     res.redirect("/");
   } else {
     res.status(400).send('Incorrect credentials. <a href = "/login">Please try again.</a>');
@@ -160,15 +128,13 @@ app.post("/login", (req, res) => {
 
 // Logout Page
 app.post("/logout", (req, res) => {
-  // res.clearCookie('user_id');
-  req.session.user_id = null;
+  req.session = null;
   res.redirect("/urls");
 });
 
 
 // URL Index
 app.get("/urls", (req, res) => {
-  // const userId = req.cookies["user_id"];
   const userId = req.session.user_id;
   const templateVars = { user: users[userId], urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -176,7 +142,6 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
-  // let userId = req.cookies["user_id"];
   let userId = req.session.user_id;
   urlDatabase[randomString] = { longURL: req.body.longURL, createdBy: userId };
   res.redirect("/urls/" + randomString);
@@ -185,10 +150,7 @@ app.post("/urls", (req, res) => {
 
 // Create a new URL
 app.get("/urls/new", (req, res) => {
-  // const userId = req.cookies["user_id"];
-  // const userId = req.session.user_id;
   const templateVars = { user: users[userId], urls: urlDatabase };
-  // let currentUser = req.cookies.user_id;
   let currentUser = req.session.user_id;
 
   if (!currentUser) {
@@ -201,7 +163,6 @@ app.get("/urls/new", (req, res) => {
 
 // Show URL
 app.get("/u/:shortURL", (req, res) => {
-  // const userId = req.cookies["user_id"];
   const userId = req.session.user_id;
   const templateVars = { user: users[userId], urls: urlDatabase };
   let longURL = urlDatabase[req.params.shortURL];
@@ -211,7 +172,6 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Update URL
 app.get("/urls/:id", (req, res) => {
-  // const userId = req.cookies["user_id"];
   const userId = req.session.user_id;
   const templateVars = { user: users[userId], urls: urlDatabase, shortURL: req.params.id, longURL: urlDatabase[req.params.id].longURL };
   res.render("urls_show", templateVars);
